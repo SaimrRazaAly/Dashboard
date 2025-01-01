2425ca147ee62dd4ea9717b95258f74fd61ded19
SaimRazaAly <saimrazateach2010@gmail.com>
2024-12-31 23:19:49 -0800

    sucessfully done all configuraiton

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react"
+import { BrowserRouter, Routes,route } from "react-router-dom"
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <BrowserRouter>
+      <div className="flex realtive dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{zIndex :'1000'}}>
+
+<TooltipComponent content={"settings"} position="Top">
+  <button>
+    <FiSettings/>
+  </button>
+
+</TooltipComponent>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </BrowserRouter>
+    </div>
   )
 }
 
